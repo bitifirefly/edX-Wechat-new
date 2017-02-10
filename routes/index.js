2d9211852e556e58a706b4a508bace047da65d61
uniquexiaobai <unique_xiaobai@163.com>
2017-02-10 14:15:01 +0800

    remove my_video

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const findCourseRouter = require('./find_courses');
 const myCourseRouter = require('./my_courses');
-const myVideosRouter = require('./my_videos');
 const settingsRouter = require('./settings');
 const signinRouter = require('./accounts/signin');
 const authWechatRouter = require('./accounts/auth_wechat');
@@ -9,7 +8,6 @@ const authMiddleware = require('../middlewares/auth');
 module.exports = (app) => {
   app.use('/', findCourseRouter);
   app.use('/my_courses', authMiddleware, myCourseRouter);
-  app.use('/my_videos', authMiddleware, myVideosRouter);
   app.use('/settings', settingsRouter);
   app.use('/signin', signinRouter);
   app.use('/auth_wechat', authWechatRouter);
